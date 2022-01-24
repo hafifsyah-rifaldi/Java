afd66efc41ff3cab16b02cff4ca3050f69d10112
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-01-24 16:36:44 +0700

    Added Package "Kendaraan" with class "Mobil, Kereta, Motor"

--- StrukturDasar/src/com/dicoding/javafundamental/basic/Main.java
+++ StrukturDasar/src/com/dicoding/javafundamental/basic/Main.java
@@ -1,10 +1,15 @@
 package com.dicoding.javafundamental.basic;
 
 import com.dicoding.javafundamental.basic.music.Gitar;
+import com.dicoding.javafundamental.basic.kendaraan.*;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         Gitar.Bunyi();
+
+        Mobil.jumlahBan();
+        Motor.jumlahBan();
+        Kereta.jumlahBan();
     }
 }
---
+++ StrukturDasar/src/com/dicoding/javafundamental/basic/kendaraan/Kereta.java
@@ -0,0 +1,9 @@
+package com.dicoding.javafundamental.basic.kendaraan;
+
+public class Kereta {
+    public static void jumlahBan() {
+        System.out.println("Ban kereta banyak!");
+        
+    }
+    
+}
