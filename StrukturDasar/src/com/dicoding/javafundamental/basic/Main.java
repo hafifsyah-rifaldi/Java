9a4973f02aa6a7c89eb107ab8be896a364935c6f
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-01-24 16:29:39 +0700

    Added new package "Music" and class "Gitar"

--- StrukturDasar/src/com/dicoding/javafundamental/basic/Main.java
+++ StrukturDasar/src/com/dicoding/javafundamental/basic/Main.java
@@ -1,7 +1,10 @@
 package com.dicoding.javafundamental.basic;
 
+import com.dicoding.javafundamental.basic.music.Gitar;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Gitar.Bunyi();
     }
 }
