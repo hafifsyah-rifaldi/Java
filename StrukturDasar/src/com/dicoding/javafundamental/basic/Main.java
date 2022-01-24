110e347829fdc12ec75fb410decc028b88fdef6d
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-01-24 16:50:57 +0700

    Added some library import "Date"

--- StrukturDasar/src/com/dicoding/javafundamental/basic/Main.java
+++ StrukturDasar/src/com/dicoding/javafundamental/basic/Main.java
@@ -2,6 +2,10 @@ package com.dicoding.javafundamental.basic;
 
 import com.dicoding.javafundamental.basic.music.Gitar;
 import com.dicoding.javafundamental.basic.kendaraan.*;
+import org.apache.commons.lang3.time.DateUtils;
+
+import java.util.Date;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -11,5 +15,10 @@ public class Main {
         Mobil.jumlahBan();
         Motor.jumlahBan();
         Kereta.jumlahBan();
+
+        Date today = new Date();
+        System.out.println("Hari ini = " + today);
+        Date tommorow = DateUtils.addDays(today, 1);
+        System.out.println("Besok = " + tommorow);
     }
 }
