1edb2c17c861224d8a9bb82a3ea0add09cf6be98
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-01-24 17:39:25 +0700

    Added class "MyString"

--- Modul1Basic/String/src/com/dicoding/javafundamental/string/MyString.java
+++ Modul1Basic/String/src/com/dicoding/javafundamental/string/MyString.java
@@ -1,3 +1,17 @@
 public class MyString {
-    
+public static void main(String[] args) {
+    char[] dicodingChars = {'d','i','c','o','d','i','n','g'};
+    String dicodingString = new String(dicodingChars);
+    System.out.println(dicodingString);
+
+
+    /* Mengetahui Panjang String */
+    String dicoding = "dicoding";
+    int length = dicoding.length();
+    System.out.println(length);
+
+    /* Mengambil Karakter dari Sebuah String */
+    char result = dicoding.charAt(6);
+    System.out.println(result);
+}    
 }
