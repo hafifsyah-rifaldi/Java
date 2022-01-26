fa2d316d5ea6f7a4669e1f6cc93d521d90b6df54
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-01-26 23:16:38 +0700

    Added class UserBufferedReader

---
+++ Modul1Basic/InputOutput/src/com/dicoding/javafundamental/inputoutput/UserBufferedReader.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class UserBufferedReader {
+
+    public static void main(String[] args) {
+        
+        InputStreamReader streamReader = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(streamReader);
+        System.out.println("Program penjumlahan sangat sederhana");
+        int value = 0;
+        int anotherValue = 0;
+        try {
+            System.out.println("Masukan Angka Pertama: ");
+            value = Integer.parseInt(bufferedReader.readLine());
+            System.out.println("Masukan Angka Kedua: ");
+            anotherValue = Integer.parseInt(bufferedReader.readLine());
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        int resul = value + anotherValue;
+        System.out.println("Hasilnya adalah: " + resul);
+    }
+}
