694990a50359e0eafb4c389f280958f4adfb15b5
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-01-26 14:35:27 +0700

    Added class InputOutputFunction

---
+++ Modul1Basic/InputOutput/src/com/dicoding/javafundamental/inputoutput/InputOutputFunction.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class InputOutputFunction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Program Penjumlahan sangat sederhana");
+        System.out.println("Masukkan Angka Pertama: ");
+        int value = scanner.nextInt();
+        System.out.println("Masukan Angka Kedua; ");
+        int anotherValue = scanner.nextInt();
+        int result = value + anotherValue;
+        System.out.println("Hasil adalah : " + result);
+    }
+}
