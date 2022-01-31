bd3181a2ac9c0d41409c57af3d9241c3a697ccc5
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-01-31 17:39:08 +0700

    Added class IfThen

---
+++ Modul1Basic/Percabangan/src/com/dicoding/javafundamental/percabangan/IfThen.java
@@ -0,0 +1,15 @@
+package com.dicoding.javafundamental.percabangan;
+
+public class IfThen {
+    public static void main(String[] args) {
+        boolean isOn = true;
+
+        // {} braces digunakan ketika ada 2 baris kode
+        if (isOn) {
+            System.out.println("Menyalakan lampu");
+            System.out.println("Menyalakan lampu lain");
+            
+        }
+    }
+    
+}
