0046b384ef6ebd59a7cb1a72c968946d184e09dc
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-01-31 20:11:13 +0700

    Added class While

---
+++ Modul1Basic/Perulangan/src/com/dicoding/javafundamental/perulangan/While.java
@@ -0,0 +1,11 @@
+public class While {
+    public static void main(String[] args) {
+        int value = 1;
+        while (value <= 10) {
+            System.out.println("Angka : " + value);
+            value++;
+            System.out.println("\n");
+            
+        }
+    }
+}
