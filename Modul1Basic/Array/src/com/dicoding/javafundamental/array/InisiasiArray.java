69e07550813bffef353d7300345c5cc62ee52c1f
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-01-31 13:46:23 +0700

    Added class InisiasiArray

---
+++ Modul1Basic/Array/src/com/dicoding/javafundamental/array/InisiasiArray.java
@@ -0,0 +1,19 @@
+public class InisiasiArray {
+    public static void main(String[] args) {
+        int[] arrInt = {1, 2, 3, 4, 5, 6};
+
+        arrInt[0] = 1;
+        arrInt[1] = 2;
+        arrInt[2] = 3;
+        arrInt[3] = 4;
+        arrInt[4] = 5;
+        arrInt[5] = 6;
+        
+        System.out.println(arrInt[0]);
+        System.out.println(arrInt[1]);
+        System.out.println(arrInt[2]);
+        System.out.println(arrInt[3]);
+        System.out.println(arrInt[4]);
+        System.out.println(arrInt[5]);
+    }
+}
