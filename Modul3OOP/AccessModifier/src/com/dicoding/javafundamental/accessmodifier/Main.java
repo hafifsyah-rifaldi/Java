eb47453c27c419d6b21c0d655533e7e847e37dec
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-02-04 13:58:33 +0700

    Added class Main dan KelasA

---
+++ Modul3OOP/AccessModifier/src/com/dicoding/javafundamental/accessmodifier/Main.java
@@ -0,0 +1,16 @@
+package com.dicoding.javafundamental.accessmodifier;
+
+public class Main {
+    public static void main(String[] args) {
+        KelasA kelasA = new KelasA();
+
+        /*System.out.println(kelasA.memberA);
+
+        System.out.println(kelasA.functionA()); 
+        
+        memberA dan functionA dalam keadaan private pada class Main.java
+        */
+
+        System.out.println(kelasA.functionB());
+    }
+}
