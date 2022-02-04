78926dd05a9fbcc211b2ece16cf23d15618db321
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-02-04 16:40:14 +0700

    Added Method

--- Modul3OOP/Inheritance/src/com/dicoding/javafundamental/inheritance/Main.java
+++ Modul3OOP/Inheritance/src/com/dicoding/javafundamental/inheritance/Main.java
@@ -19,6 +19,19 @@ public class Main {
         hewan.makan();
         kucing.makan(); // kucing mewarisi metode makan() dari parent class Hewan
         kucing.makan("daging ikan");
+
+        System.out.println("------------------------------");
+
+        Kucing meow = new Kucing("Ocicat", "tropis");
+        Kucing puss = new Kucing("Ocicat", "subtropis");
+        Kucing popo = new Kucing("Anggora", "subtropis");
+
+        System.out.println(meow.toString());
+        System.out.println(puss.toString());
+        System.out.println(popo.toString());
+
+        System.out.println("meow equals puss ? " + meow.equals(puss));
+        System.out.println("meow equals popo ? " + meow.equals(popo));
     }
     
 }
