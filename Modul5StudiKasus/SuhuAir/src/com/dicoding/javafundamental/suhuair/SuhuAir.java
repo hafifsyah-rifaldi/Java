25702b09b6aa5d9c1b7b1837e3cdf084be0e9096
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-02-08 14:19:01 +0700

    Added class StudyCase: SuhuAir.java

---
+++ Modul5StudiKasus/SuhuAir/src/com/dicoding/javafundamental/suhuair/SuhuAir.java
@@ -0,0 +1,29 @@
+package com.dicoding.javafundamental.suhuair;
+
+import java.util.Scanner;
+
+public class SuhuAir {
+    public static void main(String[] args) {
+        /* Contoh pemakaian IF tiga kasus : wujud air */
+
+        /* Kamus */
+        int T;
+
+        /* Program */
+        System.out.println("Contoh IF tiga kasus");
+        System.out.println("Suhu (der.C) = ");
+    
+        // Scanner untuk masukan suhu air
+        Scanner scanner = new Scanner(System.in);
+        T = scanner.nextInt(); // masukkan suhu air dengan tipe int
+
+        // Proses pengecekan dengan if
+        if (T < 0){
+            System.out.println("Wujud air beku " + T);
+        } else if ((0 <= T) && (T <= 00)) {
+            System.out.println("Wujud air cair " + T);
+        } else if (T > 100) {
+            System.out.println("Wujud air uap/gas " + T);
+        }
+    }
+}
